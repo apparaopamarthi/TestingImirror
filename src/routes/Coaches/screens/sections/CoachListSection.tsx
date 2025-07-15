b5c447c8213e3806c3709028a1276815eda06697
apparaopamarthi <129172501+apparaopamarthi@users.noreply.github.com>
2025-07-15 10:48:59 +0530

    Fix Avatar import path in CoachListSection

--- src/routes/Coaches/screens/sections/CoachListSection.tsx
+++ src/routes/Coaches/screens/sections/CoachListSection.tsx
@@ -4,7 +4,7 @@ import {
   Avatar,
   AvatarFallback,
   AvatarImage,
-} from "../../../../components/ui/avatar";
+} from "../../components/ui/avatar";
 import { Button } from "../../../../components/ui/button";
 import { Card, CardContent } from "../../../../components/ui/card";
 
